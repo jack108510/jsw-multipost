a02a8025392c30bf65aee419e429f8d976698940
Jack Wilde <wildejack1010@gmail.com>
2026-08-26 18:09:14 -0600

    Rename extension branding to Reachr

--- reload.js
+++ reload.js
@@ -1,8 +1,8 @@
 setTimeout(() => {
   try {
-    document.title = 'Amplr reload ' + chrome.runtime.getManifest().version;
+    document.title = 'Reachr reload ' + chrome.runtime.getManifest().version;
     chrome.runtime.reload();
   } catch (e) {
-    document.title = 'Amplr reload error ' + e.message;
+    document.title = 'Reachr reload error ' + e.message;
   }
 }, 500);
